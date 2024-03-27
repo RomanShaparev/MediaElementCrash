b1db5738dc0a00a0db15c32df068c92fa200f5f6
roman.shaparev <shaparev.roman@gmail.com>
2024-03-27 15:37:54 +0800

    hotfix

---
+++ MediaElementPage.xaml.cs
@@ -0,0 +1,18 @@
+// --------------------------------------------------------------------
+// Copyright Realityworks, Inc. 2024 (c)
+// 
+// Created in 2024 as an unpublished copyright work.
+// This program and  the information contained in it are confidential and
+// proprietary to Realityworks, Inc., and may not be used, copied,
+// or reproduced  without prior written permission of Realityworks, Inc.
+// --------------------------------------------------------------------
+
+namespace MediaElementCrash;
+
+public partial class MediaElementPage : ContentPage
+{
+    public MediaElementPage()
+    {
+        InitializeComponent();
+    }
+}
